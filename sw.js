987b22e04a918f7102ba1dbfe55124bbfde5a292
hugomanhani <hugomanhani@gmail.com>
2026-07-22 18:09:58 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "garage-wod-v24";
+const CACHE = "garage-wod-v26";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
